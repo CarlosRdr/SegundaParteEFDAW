a8432acb06e30e0f26e73dbc50822996e049c7b9
Carlos <13100138@ue.edu.pe>
2017-07-22 14:19:51 -0500

    primer commit

---
+++ EF_Practice/DAGeneral.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Practice
+{
+    public class DAGeneral
+    {
+        static public List<Marca> listadoMarca()
+        {
+            List<Marca> listado = new List<Marca>();
+            using (var data = new BDAlquilerVehiculoEntities())
+            {
+                return data.Marca.ToList();
+            }
+        }
+        static public bool RegistrarMarca(Marca Marca)
+        {
+            bool exito = true;
+            try
+            {
+                using (var data = new BDAlquilerVehiculoEntities())
+                {
+                    data.Marca.Add(Marca);
+                    data.SaveChanges();
+                }
+            }
+            catch
+            {
+
+                exito = false;
+            }
+
+            return exito;
+        }
+
+        static public bool ActualizarMarca(Marca Marca)
+        {
+            bool exito = true;
+            try
+            {
+                using (var data = new BDAlquilerVehiculoEntities())
+                {
+                    Marca actual = data.Marca.Where(x => x.CodMarca == Marca.CodMarca).FirstOrDefault();
+                    actual.CodMarca = Marca.CodMarca;
+                    actual.Descripcion = Marca.Descripcion;
+                    actual.Pais = Marca.Pais;
+                    
+                    data.SaveChanges();
+                }
+
+            }
+            catch (Exception)
+            {
+
+                exito = false;
+            }
+            return exito;
+        }
+        static public bool EliminarMarca(Marca Marca)
+        {
+            bool exito = true;
+            try
+            {
+                using (var data = new BDAlquilerVehiculoEntities())
+                {
+                    Marca actual = data.Marca.Where(x => x.CodMarca == Marca.CodMarca).FirstOrDefault();
+                    data.Marca.Remove(actual);
+                    data.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+
+                exito = false;
+            }
+            return exito;
+        }
+
+    }
+}
---
+++ Practicando/Areas/Marca/Controllers/MainController.cs
@@ -0,0 +1,54 @@
+﻿using EF_Practice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practicando.Areas.Marca.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Marca/Main
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Listado()
+
+        {
+            return PartialView(DAGeneral.listadoMarca());
+        }
+        public ActionResult Eliminar(string ID)
+        {
+            EF_Practice.Marca marca = DAGeneral.listadoMarca().Where(x => x.CodMarca == ID).FirstOrDefault();
+            bool exito = DAGeneral.EliminarMarca(marca);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Editar(string ID)
+        {
+            EF_Practice.Marca marca = DAGeneral.listadoMarca().Where(x => x.CodMarca == ID).FirstOrDefault();
+            return View(marca);
+        }
+
+
+        [HttpPost]
+        public ActionResult Editar(EF_Practice.Marca marca)
+        {
+            bool exito = DAGeneral.ActualizarMarca(marca);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Crear()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Crear(EF_Practice.Marca marca)
+        {
+            bool exito = DAGeneral.RegistrarMarca(marca);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
