c5666e3e4fe852e93209be3bf53e2eb126a975e7
alexisqc92 <alexisquezadacas@gmail.com>
2017-07-23 21:09:33 -0500

    Listado, Crea,Edita

--- Practicando/Areas/Reserva/Controllers/MainController.cs
+++ Practicando/Areas/Reserva/Controllers/MainController.cs
@@ -18,5 +18,39 @@ namespace Practicando.Areas.Reserva.Controllers
         {
             return PartialView((DAGeneral.listadoReserva()));
         }
+        public ActionResult Editar(int ID)
+        {
+            EF_Practice.Reserva Reserva = DAGeneral.listadoReserva().Where(x => x.CodReserva == ID).FirstOrDefault();
+            return View(Reserva);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(EF_Practice.Reserva Reserva)
+        {
+            bool exito = DAGeneral.ActualizarReserva(Reserva);
+            return RedirectToAction("Index");
+        }
+
+       
+        public ActionResult Crear()
+        {
+            ViewBag.ListadoReservas = DAGeneral.listadoReserva();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Crear(EF_Practice.Reserva Reserva)
+        {
+            bool exito = DAGeneral.RegistrarReserva(Reserva);
+            return RedirectToAction("Index");
+        }
+
+       
+        public ActionResult Eliminar(int ID)
+        {
+            EF_Practice.Reserva Reserva = DAGeneral.listadoReserva().Where(x => x.CodReserva == ID).FirstOrDefault();
+            bool exito = DAGeneral.EliminarReserva(Reserva);
+            return RedirectToAction("Reservas");
+        }
+
     }
 }
